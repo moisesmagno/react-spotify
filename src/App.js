661a52c45824ccefaba26271e24a0f7d49839fe0
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2019-03-28 11:11:59 -0300

    Configurado o index.js da pasta store, configurado basicamente os index.js das pastas ducks e sagas.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './config/reactotron';
 
@@ -12,23 +13,26 @@ import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 
 import Routes from './routes';
+import store from "./store";
 
 const App = () => (
-	<BrowserRouter>
-		<Fragment>
-			<GlobalStyle />
-			<Wrapper>
-				<Container>
-					<Sidebar />
-					<Content>
-						<Header />
-						<Routes />
-					</Content>
-				</Container>
-				<Player />
-			</Wrapper>
-		</Fragment>
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<Fragment>
+				<GlobalStyle />
+				<Wrapper>
+					<Container>
+						<Sidebar />
+						<Content>
+							<Header />
+							<Routes />
+						</Content>
+					</Container>
+					<Player />
+				</Wrapper>
+			</Fragment>
+		</BrowserRouter>
+	</Provider>
 );
 
 export default App;
